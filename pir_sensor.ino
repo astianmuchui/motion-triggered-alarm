b27e1d27f5f54d2971d1d5db175e330377ab1f25
Sebastian Muchui <Sebastianmuchui79@gmail.com>
2022-07-06 08:18:29 +0300

    Added a function

--- pir_sensor.ino
+++ pir_sensor.ino
@@ -1,17 +1,28 @@
 int ledpin = 12;
 int vcc = 15;
 int status;
+
+
+
 void setup(){
   pinMode(ledpin, OUTPUT);
   pinMode(vcc, INPUT);
   Serial.begin(9600);
 }
 void loop(){
-  status = digitalRead(vcc);
-  if(status == HIGH){
+  if(DetectMotion()== true){
     Serial.println("Gatchu!");
     digitalWrite(ledpin, HIGH);
     delay(500);
     digitalWrite(ledpin,LOW);
+  }
+}
+// Function to detect motion
+bool DetectMotion(){
+  status = digitalRead(vcc);
+  if(status ==  HIGH){
+    return true;
+  }else{
+    return false;
   }
 }

88e6f76669a38ee7887ee7610011afcf7e7b174c
Sebastian Muchui <Sebastianmuchui79@gmail.com>
2022-08-09 11:57:29 +0300

    Relevant changes

--- main_alarm.ino
+++ main_alarm.ino
@@ -1,20 +1,23 @@
 int ledpin = 12;
 int vcc = 15;
 int status;
-
+int buzzer = 8;
 
 
 void setup(){
   pinMode(ledpin, OUTPUT);
   pinMode(vcc, INPUT);
+  pinMode(buzzer, OUTPUT);
   Serial.begin(9600);
 }
 void loop(){
   if(DetectMotion()== true){
     Serial.println("Gatchu!");
     digitalWrite(ledpin, HIGH);
+    digitalWrite(buzzer, HIGH);
     delay(500);
     digitalWrite(ledpin,LOW);
+    digitalWrite(buzzer,LOW);
   }
 }
 // Function to detect motion
